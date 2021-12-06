c0fdd0cbe55062cc9692e109291757ee7b8c6a65
ghelpo <m.f.ragab5890@gmail.com>
2021-12-06 09:37:58 +0200

    Create AppBody.js

---
+++ frontend/src/components/AppBody.js
@@ -0,0 +1,320 @@
+import React, { Component } from 'react'
+import { Grid, Segment } from 'semantic-ui-react'
+import AppMenu from './AppMenu'
+
+export default class AppBody extends Component {
+  render() {
+    return (
+      <Grid>
+        <Grid.Column width={4}>
+          <AppMenu />
+        </Grid.Column>
+        <Grid.Column stretched width={12}>
+          <Segment>
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab heightThis is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab heightThis is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab heightThis is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab heightThis is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab heightThis is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height.
+
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          This is an stretched grid column. This segment will always match the
+          tab height
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
