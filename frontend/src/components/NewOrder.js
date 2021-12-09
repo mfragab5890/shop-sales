107ec822ae25fc45b375e34a740f9913f48e0368
ghelpo <m.f.ragab5890@gmail.com>
2021-12-09 18:39:10 +0200

    Create NewOrder.js

---
+++ frontend/src/components/NewOrder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Segment } from 'semantic-ui-react'
+
+export default class NewOrder extends Component {
+
+  render() {
+
+    const { theme, lang } = this.props
+    const myScript = {}
+    return (
+      <Segment>
+        NewOrder
+      </Segment>
+    )
+  }
+}
