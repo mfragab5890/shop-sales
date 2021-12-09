efbed8e7af47ec9e34748399a1d0b68ba79f0eae
ghelpo <m.f.ragab5890@gmail.com>
2021-12-09 18:39:02 +0200

    Update AppMenu.js

--- frontend/src/components/AppMenu.js
+++ frontend/src/components/AppMenu.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom'
 
-export default class AppMenu extends Component {
-  state = { activeItem: 'sales' }
+class AppMenu extends Component {
+  state = { activeItem: 'Sales' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = async (e, { name }) => {
+    await this.setState({ activeItem: name })
+    if (name === 'Sales') {
+      return this.props.history.push('/sales')
+    }
+    if (name === 'Financial') {
+      return this.props.history.push('/financial')
+    }
+    if (name === 'Products') {
+      return this.props.history.push('/products')
+    }
+
+  }
 
   render() {
     const { activeItem } = this.state
-
+    const { theme, lang } = this.props
+    const myScript = {
+      AR: {
+        firstTab: 'المبيعات',
+        secondTab: 'المالية',
+        thirdTab: 'المنتجات'
+      },
+      EN:{
+        firstTab: 'Sales',
+        secondTab: 'Financial',
+        thirdTab: 'Products'
+      }
+    }
     return (
-      <Menu inverted icon='labeled' compact vertical pointing >
+      <Menu inverted = {theme !== 'basic'? true : false} color = {theme} icon='labeled' compact vertical pointing >
         <Menu.Item
-          name='sales'
-          active={activeItem === 'sales'}
+          name={myScript.EN.firstTab}
+          active={activeItem === 'Sales'}
           onClick={this.handleItemClick}
         >
           <Icon name='shopping cart' />
-          Sales
+          {myScript[lang].firstTab}
         </Menu.Item>
 
         <Menu.Item
-          name='financial'
-          active={activeItem === 'financial'}
+          name={myScript.EN.secondTab}
+          active={activeItem === 'Financial'}
           onClick={this.handleItemClick}
         >
           <Icon name='calculator' />
-          Financial
+          {myScript[lang].secondTab}
         </Menu.Item>
 
         <Menu.Item
-          name='products'
-          active={activeItem === 'products'}
+          name={myScript.EN.thirdTab}
+          active={activeItem === 'Products'}
           onClick={this.handleItemClick}
         >
           <Icon name='gem' />
-          Products
+          {myScript[lang].thirdTab}
         </Menu.Item>
       </Menu>
     )
   }
 }
+
+export default withRouter(AppMenu)
