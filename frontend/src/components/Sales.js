07ad4fcfc0bb4efa190873bcb1c14a1b7627131c
ghelpo <m.f.ragab5890@gmail.com>
2021-12-09 18:38:26 +0200

    Create Sales.js

---
+++ frontend/src/components/Sales.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { withRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { Segment, Menu } from 'semantic-ui-react'
+import NewOrder from './NewOrder'
+import UserSales from './UserSales'
+import Error from './Error'
+
+class Sales extends Component {
+  state = { activeItem: 'New Order' }
+
+  handleItemClick = async (e, { name }) => {
+    await this.setState({ activeItem: name })
+    if (name === 'New Order') {
+      return this.props.history.push('/sales/new')
+    }
+    if (name === 'My Sales') {
+      return this.props.history.push('/sales/mysales')
+    }
+
+  }
+  render() {
+    const { activeItem } = this.state
+    const { theme, lang } = this.props
+    const myScript = {
+      AR: {
+        firstTab: 'فاتورة جديدة',
+        secondTab: 'مبيعاتي',
+      },
+      EN:{
+        firstTab: 'New Order',
+        secondTab: 'My Sales',
+      }
+    }
+    return (
+      <Segment>
+
+        <Menu inverted = {theme !== 'basic'? true : false} color={theme} attached='top' pointing>
+          <Menu.Item
+            name= {myScript.EN.firstTab}
+            active={activeItem === myScript.EN.firstTab}
+            onClick={this.handleItemClick}
+          >
+            {myScript[lang].firstTab}
+          </Menu.Item>
+          <Menu.Item
+            name= {myScript.EN.secondTab}
+            active={activeItem === myScript.EN.secondTab}
+            onClick={this.handleItemClick}
+          >
+            {myScript[lang].secondTab}
+          </Menu.Item>
+        </Menu>
+
+        <Segment color={theme}>
+          <Switch>
+            <Route exact path='/sales'>
+              <Redirect to = '/sales/new' />
+            </Route>
+            <Route exact path='/sales/new'>
+              <NewOrder theme = {theme} lang = {lang}/>
+            </Route>
+            <Route exact path='/sales/mysales'>
+              <UserSales theme = {theme} lang = {lang}/>
+            </Route>
+            <Route path="*">
+              <Error message = {'No Such page Please Go Back Or Navigate Using Menus'}/>
+            </Route>
+          </Switch>
+        </Segment>
+
+      </Segment>
+    )
+  }
+}
+
+export default withRouter(Sales)
