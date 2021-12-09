96634249820b4e26c3d756811761a0b445698a91
ghelpo <m.f.ragab5890@gmail.com>
2021-12-09 18:39:19 +0200

    Create SalesToday.js

---
+++ frontend/src/components/SalesToday.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Segment } from 'semantic-ui-react'
+
+export default class SalesToday extends Component {
+
+  render() {
+
+    const { theme, lang } = this.props
+    const myScript = {}
+    return (
+      <Segment>
+        SalesToday
+      </Segment>
+    )
+  }
+}
