c5e08101923cb1570dc9863a7209e5aa0a0998d6
ghelpo <m.f.ragab5890@gmail.com>
2021-12-09 18:38:53 +0200

    Update AppFooter.js

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { Header, Image, Segment, Grid } from 'semantic-ui-react'
+import { Header, Image, Grid } from 'semantic-ui-react'
 
-const AppFooter = () => (
-  <segment>
-    <Grid centered columns = {3} textAlign='center' verticalAlign='middle'>
-      <Grid.Row centered stretched color='black'>
-        <Grid.Column width={3} >
-          <Image circular size = 'mini' src='/logo.png' centred />
-        </Grid.Column>
-        <Grid.Column width={9}>
-          <Header as='h1' inverted color='grey' textAlign='center' >
-            Fiori Store
-          </Header>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Header as='h4' inverted color='grey' floated = 'right' textAlign='center' >
-
-            Powerd by MFT
-          </Header>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </segment>
-)
+const AppFooter = (props) => {
+  const { theme } = props
+  return (
+    <div>
+      <Grid centered columns = {3} textAlign='center' verticalAlign='middle'>
+        <Grid.Row centered stretched color={theme}>
+          <Grid.Column width={3} >
+            <Image circular size = 'mini' src='/logo.png' centered />
+          </Grid.Column>
+          <Grid.Column width={9}>
+            <Header as='h1' inverted = {theme !== 'basic'? true : false} color={theme !== 'basic'? 'grey' : 'black'} textAlign='center' >
+              Fiori Store
+            </Header>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Header as='h3' inverted = {theme !== 'basic'? true : false} color={'grey'} floated = 'right' textAlign='center' >
+              Powerd by MFT
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
 
 export default AppFooter
