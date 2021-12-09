b2e2f94c419200cf0c7863ef07c428852ac0bbee
ghelpo <m.f.ragab5890@gmail.com>
2021-12-09 18:39:16 +0200

    Create UserSales.js

---
+++ frontend/src/components/UserSales.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Segment } from 'semantic-ui-react'
+
+export default class UserSales extends Component {
+
+  render() {
+
+    const { theme, lang } = this.props
+    const myScript = {}
+    return (
+      <Segment>
+        UserSalesToday
+      </Segment>
+    )
+  }
+}
