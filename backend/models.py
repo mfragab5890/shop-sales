d0229a82c8059eec07eac4c0c732ca92ed07ec21
ghelpo <m.f.ragab5890@gmail.com>
2021-12-12 17:29:49 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -31,6 +31,7 @@ class User(db.Model):
     products = db.relationship('Products', backref='user_products', lazy=True, cascade="all, delete-orphan")
     permissions = db.relationship('UserPermissions', backref='user_permissions', lazy=True,
                                   cascade="all, delete-orphan")
+    orders = db.relationship('Orders', backref='user_sales', lazy=True, cascade="all, delete-orphan")
 
     def __init__(self, user_name, email, password):
         self.user_name = user_name
@@ -70,6 +71,7 @@ class Permissions(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
+    holders = db.relationship('UserPermissions', backref='permission_holders', lazy=True, cascade="all, delete-orphan")
 
     def __init__(self, name):
         self.name = name
@@ -98,10 +100,10 @@ class UserPermissions(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    permission_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    permission_id = db.Column(db.Integer, db.ForeignKey('permissions.id'), nullable=False)
 
     def __init__(self, user_id, permission_id):
-        self.user_name = user_id
+        self.user_id = user_id
         self.permission_id = permission_id
 
     def insert(self):
@@ -131,6 +133,8 @@ class Products(db.Model):
     name = db.Column(db.String, nullable=False)
     code = db.Column(db.String, unique=True, nullable=False)
     qty = db.Column(db.Integer, default=0)
+    sell_price = db.Column(db.Integer, nullable=False)
+    buy_price = db.Column(db.Integer, nullable=False)
     created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     mini = db.Column(db.Integer, default=0)
     maxi = db.Column(db.Integer)
@@ -163,6 +167,85 @@ class Products(db.Model):
             'id': self.id,
             'name': self.name,
             'code': self.code,
+            'qty': self.qty,
+            'created_by': self.created_by,
+            'mini': self.mini,
+            'maxi': self.maxi,
+            'sold': self.sold,
+            'image': self.image,
+        }
+
+
+# orders table
+class Orders(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    qty = db.Column(db.Integer, default=0)
+    total_price = db.Column(db.Integer, nullable=False)
+    total_cost = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    items = db.relationship('OrderItems', backref='items', lazy=True, cascade="all, delete-orphan")
+
+    def __init__(self, qty, created_by, total_price, total_cost):
+        self.qty = qty
+        self.total_price = total_price
+        self.total_cost = total_cost
+        self.created_by = created_by
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'qty': self.qty,
+            'created_by': self.created_by,
+            'total_price': self.total_price,
+            'total_cost': self.total_cost,
+        }
+
+
+# order items table
+class OrderItems(db.Model):
+    __tablename__ = 'order_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    qty = db.Column(db.Integer, default=0)
+    total_price = db.Column(db.Integer, nullable=False)
+    total_cost = db.Column(db.Integer, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+
+    def __init__(self, qty, total_price, total_cost, order_id, product_id):
+        self.qty = qty
+        self.total_price = total_price
+        self.total_cost = total_cost
+        self.order_id = order_id
+        self.product_id = product_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
             'qty': self.qty,
             'created_by': self.created_by,
             'mini': self.mini,
