cde723c41981c27978b023ed6f6664a8101b428a
ghelpo <m.f.ragab5890@gmail.com>
2022-01-11 12:44:48 +0200

    Update index.js
    add redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from "./reducers"
+import middleware from './middleware'
 import {BrowserRouter} from 'react-router-dom'
 
+const store = createStore(reducer, middleware)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
